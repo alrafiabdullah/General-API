e87addd851cea8979bdd9708799577c42cc2b61e
alrafiabdullah <abdullah34alrafi@gmail.com>
2021-09-21 16:57:16 +0600

    exception handled

--- ses_email/views.py
+++ ses_email/views.py
@@ -18,24 +18,21 @@ class SesEmail(APIView):
         return Response({"status": "Email Service Up & Running!"}, status=status.HTTP_200_OK)
 
     def post(self, request):
-        try:
-            api_key = request.data['api_key']
+        api_key = request.data['api_key']
 
-            if not VerificationCode.objects.filter(code=api_key, validity=True):
-                return Response({"error": f"Invalid API Key, {api_key}\\nEither expired or does not exist!"}, status=status.HTTP_400_BAD_REQUEST)
+        if not VerificationCode.objects.filter(code=api_key, validity=True):
+            return Response({"error": f"Invalid API Key, {api_key}\\nEither expired or does not exist!"}, status=status.HTTP_400_BAD_REQUEST)
 
-            serializer = self.serializer_class(data=request.data)
-            serializer.is_valid(raise_exception=True)
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
 
-            email = EmailMessage(
-                subject=f"Contact Form - {serializer.data['name']}",
-                body=serializer.data["message"],
-                from_email=settings.EMAIL_FROM,
-                to=[serializer.data["email"]],
-                bcc=[settings.EMAIL_BCC],
-            )
+        email = EmailMessage(
+            subject=serializer.validated_data['subject'],
+            body=serializer.validated_data["message"],
+            from_email=settings.EMAIL_FROM,
+            to=[serializer.validated_data["email"]],
+            bcc=[settings.EMAIL_BCC],
+        )
 
-            email.send()
-            return Response({"message": f"Email sent to {serializer.data['email']}"}, status=status.HTTP_200_OK)
-        except:
-            return Response({"error": "Something went wrong!"}, status=status.HTTP_400_BAD_REQUEST)
+        email.send()
+        return Response({"message": f"Email sent to {serializer.validated_data['email']}"}, status=status.HTTP_200_OK)
