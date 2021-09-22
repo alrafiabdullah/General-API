681a6d64ebde7fbfcd57c56cd715db4f9634e310
alrafiabdullah <abdullah34alrafi@gmail.com>
2021-09-22 13:56:10 +0600

    rate limit functionality added

--- ses_email/views.py
+++ ses_email/views.py
@@ -5,6 +5,9 @@ from rest_framework.views import APIView
 from django.conf import settings
 from django.core.mail import EmailMessage
 
+from ratelimit.core import get_usage
+
+
 from .models import VerificationCode
 from .serializers import EmailSerializer
 
@@ -15,13 +18,29 @@ class SesEmail(APIView):
     serializer_class = EmailSerializer
 
     def get(self, request):
+        usage = get_usage(request, group="email_ratelimit", fn=self.get, key="ip",
+                          rate="5/h", method="GET", increment=True)
+
+        if usage["should_limit"]:
+            return Response({"status": "Rate limit exceeded", "current_limit": usage["limit"], "total_sent": usage["count"], "time_left": usage["time_left"]}, status=status.HTTP_429_TOO_MANY_REQUESTS)
+
         return Response({"status": "Email Service Up & Running!"}, status=status.HTTP_200_OK)
 
     def post(self, request):
-        api_key = request.data['api_key']
+        try:
+            api_key = request.data['api_key']
+            if not VerificationCode.objects.filter(code=api_key, validity=True):
+                return Response({"error": f"Invalid API Key, {api_key}\\nEither expired or does not exist!"}, status=status.HTTP_400_BAD_REQUEST)
+            user_rate_limit = VerificationCode.objects.get(
+                code=api_key).rate_limit
+        except:
+            return Response({"status": "Missing API Key"}, status=status.HTTP_400_BAD_REQUEST)
 
-        if not VerificationCode.objects.filter(code=api_key, validity=True):
-            return Response({"error": f"Invalid API Key, {api_key}\\nEither expired or does not exist!"}, status=status.HTTP_400_BAD_REQUEST)
+        usage = get_usage(request, group="email_ratelimit", fn=self.post, key="ip",
+                          rate=f"{user_rate_limit}/h", method="POST", increment=True)
+
+        if usage["should_limit"]:
+            return Response({"status": "Rate limit exceeded", "current_limit": usage["limit"], "total_sent": usage["count"], "time_left": usage["time_left"]}, status=status.HTTP_429_TOO_MANY_REQUESTS)
 
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
