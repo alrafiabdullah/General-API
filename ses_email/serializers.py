362ecfceb0be6b34847f55fce534eec28765f7e7
alrafiabdullah <abdullah34alrafi@gmail.com>
2021-09-23 14:25:15 +0600

    email field removed

--- ses_email/serializers.py
+++ ses_email/serializers.py
@@ -2,6 +2,5 @@ from rest_framework import serializers
 
 
 class EmailSerializer(serializers.Serializer):
-    email = serializers.EmailField()
     subject = serializers.CharField(max_length=100)
     message = serializers.CharField(max_length=999)
