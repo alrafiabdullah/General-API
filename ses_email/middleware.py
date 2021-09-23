e5dced9f2f5ed0146df1b19ddd8deeb6fff7778c
alrafiabdullah <abdullah34alrafi@gmail.com>
2021-09-23 13:16:50 +0600

    middleware updated

--- ses_email/middleware.py
+++ ses_email/middleware.py
@@ -2,14 +2,12 @@ from django.utils.deprecation import MiddlewareMixin
 
 
 class XForwardedForMiddleware(MiddlewareMixin):
-    def __init__(self, get_response):
-        self.get_response = get_response
+    # def __init__(self, get_response):
+    #     self.get_response = get_response
 
-    def __call__(self, request):
-        return self.get_response(request)
+    # def __call__(self, request):
+    #     return self.get_response(request)
 
     def process_request(self, request):
         if "HTTP_X_FORWARDED_FOR" in request.META:
-            request.META["HTTP_X_PROXY_REMOTE_ADDR"] = request.META["REMOTE_ADDR"]
-            parts = request.META["HTTP_X_FORWARDED_FOR"].split(",", 1)
-            request.META["REMOTE_ADDR"] = parts[0]
+            request.META["REMOTE_ADDR"] = request.META["HTTP_X_FORWARDED_FOR"]
